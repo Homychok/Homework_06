cf17a43cd4267966d8425bb02ba08b2449547b0c
Alexandra <alexandravolchihina@yandex.ru>
2022-12-14 09:43:58 +0300

    Домашнее задание 6 сделано

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        homework1();
+
+    }
+    public static void homework1() {
+        System.out.println("Домашняя задача 1");
+        {
